13e0a292725b894f27a058d06524c39ba4672dd6
MasterTextman <mctech26@gmail.com>
2019-03-23 15:33:46 +0100

    Added comments

--- bin/mutations.h
+++ bin/mutations.h
@@ -18,14 +18,23 @@ class Mutation {
     private:
         uniform_int_distribution<size_t> dist;
         int iter;
+        // mutates chunks of bytes
         void mutchunks(string& contents);
+        // repeats chunks of bytes
         void mutrepeat(string& contents);
+        // removes chunks of bytes
         void mutremove(string& contents);
+        // randomly changes bytes
         void mutscatter(string& contents);
+        // reverses chunks of bytes
         void mutreverse(string& contents);
+        // moves chunks of bytes around
         void mutmove(string& contents);
+        // randomly sets bytes to 0
         void mutzero(string& contents);
+        // swaps chunks of bytes around
         void mutswap(string& contents);
+        // swaps chunks of bytes around - uses iteration - legacy
         void mutiswap(string& contents);
         char randomASCII();
 };
